c98737b95aac360b09194dd0581a0484d67e3d51
Anne Wilson <annewilson2591@gmail.com>
2019-02-07 15:05:06 -0700

    add onclick function

--- script.js
+++ script.js
@@ -2,14 +2,16 @@
 //https://console.firebase.google.com/u/1/project/train-scheduler-5e7e0/database/train-scheduler-5e7e0/data
 
 
+
+
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyBk777sSwDWry1XVuCqwHVALAPxtRonCJk",
-    authDomain: "train-scheduler-66329.firebaseapp.com",
-    databaseURL: "https://train-scheduler-66329.firebaseio.com",
-    projectId: "train-scheduler-66329",
-    storageBucket: "",
-    messagingSenderId: "832858192271"
+    apiKey: "AIzaSyBntwWsWr4bjLvHgru9FHsAxpl2TW40osM",
+    authDomain: "train-scheduler-5e7e0.firebaseapp.com",
+    databaseURL: "https://train-scheduler-5e7e0.firebaseio.com",
+    projectId: "train-scheduler-5e7e0",
+    storageBucket: "train-scheduler-5e7e0.appspot.com",
+    messagingSenderId: "793489425273"
   };
   firebase.initializeApp(config);
 
@@ -22,51 +24,68 @@
   var frequency = 0;
 
   //capture button click
-  $("submit-train").on("click", function(event) {
+  $("#submit-train").on("click", function(event) {
       event.preventDefault(); 
 
-    addTrain = $("add-train").val().trim();
-    destination = $("destination").val().trim();
-    firstTrain = $("first-train").val().trim();
-    frequency = $("frequency").val().trim();
+    //grabs user input 
+    addTrain = $("#add-train").val().trim();
+    destination = $("#destination").val().trim();
+    firstTrain = moment($("#first-train").val().trim(), "hh:mm").subtract(1, "years").format("x");
+    frequency = $("#frequency").val().trim();
 
-    //code for push
-    dataRef.ref().push({
+    //current time
+    var currentTime = moment();
+    console.log("Current Time: " + moment(currentTime).format("hh:mm"));
 
-        addTrain: addTrain,
-        destination: destination,
-        firstTrain: firstTrain,
-        frequency: frequency, 
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-    });
+    console.log(addTrain);
+
+
+    //new train info
+    var newTrain = {
+      addTrain: addTrain,
+      destination: destination,
+      firstTrain: firstTrain,
+      frequency: frequency, 
+    };
+
+       //code for push to firebase
+      dataRef.ref().push({
+      });
+  
+      //clears elements
+      $("#add-train").val("");
+      $("destination").val("");
+      $("#frequency").val("");
+
+      //why is return false needed?
+      return false;
+  
+  //end of onclick function 
   });
 
-  //firebase watcher + initial loader
+
+  //firebase watcher + initial loader 
   dataRef.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val().addTrain);
-    console.log(childSnapshot.val().destination);
-    console.log(childSnapshot.val().firstTrain);
-    console.log(childSnapshot.val().frequency);
+  console.log(childSnapshot.val().addTrain);
+  console.log(childSnapshot.val().destination);
+  console.log(childSnapshot.val().firstTrain);
+  console.log(childSnapshot.val().frequency);
+
+
+  //calculate difference between trains ****
+  var difference = moment().diff(moment(firstTrain), "minutes");
+
+  //time apart(remainder)
+  var trainRemain = difference % frequency;
+
+  //minutes until arrival
+  var minUntil = frequency - trainRemain;
+
+  //next train arrival
+  var nextArrival = moment().add(minUntil, "minutes").format('hh:mm');
   
-    // // full list of items to the well
-    // $("#full-member-list").append("<div class='well'><span class='member-name'> " +
-    //     childSnapshot.val().name +
-    //     " </span><span class='member-email'> " + childSnapshot.val().email +
-    //     " </span><span class='member-age'> " + childSnapshot.val().age +
-    //     " </span><span class='member-comment'> " + childSnapshot.val().comment +
-    //     " </span></div>");
-
-    // Handle the errors
-    }, function(errorObject) {
-        console.log("Errors handled: " + errorObject.code);
-    });
-
-    // dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-    //     // Change the HTML to reflect
-    //     $("#name-display").text(snapshot.val().name);
-    //     $("#email-display").text(snapshot.val().email);
-    //     $("#age-display").text(snapshot.val().age);
-    //     $("#comment-display").text(snapshot.val().comment);
-    //});
+  //adding new train to table
+  $("#trainTable").append("<tr><td>" + addTrain + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minUntil + "/td></tr>");
 
 
+  });
